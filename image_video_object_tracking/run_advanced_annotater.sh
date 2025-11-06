3c83802370b5444d5141cfb0d085fde9fce6a365
s0um0r0y <soumoroy09@gmail.com>
2025-11-06 12:13:45 +0530

    [REFACTOR]: added container paths

--- image_video_object_tracking/run_advanced_annotater.sh
+++ image_video_object_tracking/run_advanced_annotater.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-PCD_DIR=""
-IMAGE_DIR=""
-CONFIG=""
-OUTPUT_DIR=""
+PCD_DIR="/home/container_user/wheelchair2/src/image_video_object_tracking/pcd"
+IMAGE_DIR="/home/container_user/wheelchair2/src/image_video_object_tracking/equirectangular"
+CONFIG="/home/container_user/wheelchair2/src/config/config.yml"
+OUTPUT_DIR="my_results_v6"
 
 python3 advanced_annotater_v3.py \
     --data "$IMAGE_DIR" \
